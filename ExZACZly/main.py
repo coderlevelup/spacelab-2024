30660d0a1d8bccea96d47b44e48b76fa8c47e4d2
David Campey <campey@gmail.com>
2024-02-18 19:50:12 +0200

    Call ISS Speedy from main

--- ExZACZly/main.py
+++ ExZACZly/main.py
@@ -3,6 +3,7 @@ from logzero import logger, logfile
 from time import sleep
 from random import random
 import merliandrocam
+import ISS_speedy
 
 logfile("spacelab.log")
 
@@ -28,8 +29,20 @@ def write_result(result):
         
 
 def estimate_speed(run_number):
-    merliandrocam.capture_images(run_number, 7)
-    return 7 + random()
+    photos = merliandrocam.capture_images(run_number, 7)
+    if not photos:
+        logger.error("No filenames returned") 
+        return
+    image_files_a = photos[:-1]
+    image_files_b = photos[1:]
+    pairs = list(zip(image_files_a, image_files_b))
+    pair_speeds = []
+    for pair in pairs:
+        logger.debug(f'Calculating speed for {pair[0]}, {pair[1]}')
+        speed = ISS_Speedy.incredible_snake_sky_speedy(pair[0], pair[1])
+        print(speed)
+        pair_speeds.append(speed)
+    return sum(pair_speeds) / len(pair_speeds)
 
 estimates = []
 for i in range(6):
@@ -37,9 +50,16 @@ for i in range(6):
     # come up with next estimate
     new_estimate = estimate_speed(i)
     logger.info(f"Estimate {i+1}: {new_estimate}")
-    estimates.append(new_estimate)
-    average_estimate = sum(estimates)/len(estimates)
+
+    if new_estimate:
+        estimates.append(new_estimate)
+        
     # write every time we have a new estimate in case program crashes
-    write_result(average_estimate)
-    sleep(1)
+    if len(estimates) > 0:
+        average_estimate = sum(estimates)/len(estimates)
+        write_result(average_estimate)
+    else:
+        logger.error("Ne estimates calculated")
+        write_result(0)
+
     
