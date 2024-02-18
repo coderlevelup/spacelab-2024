50de076f36c5a8bb5703c31957900bbf22b09d67
David Campey <campey@gmail.com>
2024-02-18 16:51:48 +0200

    adding play py files, because why now

---
+++ play/modules.py
@@ -0,0 +1,18 @@
+# from logzero import logger
+# 
+# logger.debug("hello")
+# logger.info("info")
+# logger.warning("warning")
+# logger.error("error")
+# 
+# import pandas as pd
+# 
+# df = pd.read_csv("my_test_data.csv")
+# df.describe()
+
+# Python3 program to demonstrate
+# the use of random() function .
+# import random
+from random import random
+# Prints random item
+print(random())
